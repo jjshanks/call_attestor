c85498ec06b135526ea43686379ead7b1c836068
Joshua Shanks <jjshanks@meshplayground.com>
2018-09-16 00:01:05 +0000

    Initial check in of call attestor

---
+++ README.md
@@ -0,0 +1,8 @@
+# Call Attestor
+This is an example of using a unix socket to validate a caller.
+
+# Build
+`go build call_attestor.go`
+
+# Run
+`./call_attestor -socket_path /tmp/my_socket`
---
+++ call_attestor.go
@@ -0,0 +1,75 @@
+package main
+
+import (
+    "fmt"
+    "flag"
+    "net"
+    "net/http"
+    "syscall"
+    "os"
+    "os/user"
+    "github.com/shirou/gopsutil/process"
+)
+
+type MyHandler struct{}
+
+func (h *MyHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	hj, ok := w.(http.Hijacker)
+	if !ok {
+		http.Error(w, "webserver doesn't support hijacking", http.StatusInternalServerError)
+		return
+	}
+	conn, bufrw, err := hj.Hijack()
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+	// Don't forget to close the connection:
+	defer conn.Close()
+	uconn, ok := conn.(*net.UnixConn)
+	if !ok {
+		http.Error(w, "boom", http.StatusInternalServerError)
+		return
+	}
+
+	file, err := uconn.File()
+	if err != nil {
+                http.Error(w, err.Error(), http.StatusInternalServerError)
+                return
+	}
+	defer file.Close()
+
+	ucred, err := syscall.GetsockoptUcred(int(file.Fd()), syscall.SOL_SOCKET, syscall.SO_PEERCRED)
+	if err != nil {
+                http.Error(w, err.Error(), http.StatusInternalServerError)
+                return
+	}
+
+	pid := int32(ucred.Pid)
+
+	proc, _ := process.NewProcess(pid)
+	uids, err := proc.Uids()
+	if err != nil {
+		fmt.Fprintf(bufrw, err.Error())
+	}
+	u, err := user.LookupId(fmt.Sprint(uids[1]))
+	fmt.Fprintf(bufrw, "Hi there, you are pid %d, uid %d, and name %s!", pid, uids[1], u.Username)
+	fmt.Fprintln(bufrw)
+	bufrw.Flush()
+}
+
+func main() {
+	server := http.Server {
+		Handler: &MyHandler{},
+	}
+	socketPath := flag.String("socket_path", "/tmp/foo/bar", "path for socket")
+	flag.Parse()
+	os.Remove(*socketPath)
+        unixListener, err := net.Listen("unix", *socketPath)
+	defer unixListener.Close()
+	os.Chmod(*socketPath, 0777)
+        if err != nil {
+                panic(err)
+        }
+        server.Serve(unixListener)
+}
